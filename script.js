5052dd5ab32d8ec57f53b443d5bc72fb070c4d3f
Andrew Smith <Andrewwilliamjohnsmith@hotmail.co.uk>
2023-11-01 16:31:15 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const displayKey = document.querySelector(".key h2");
+const displayKeyCode = document.querySelector(".keyCode h2");
+const keyCodeDiv = document.querySelector(".keyCode");
+
+window.addEventListener("keydown", (e) => {
+  displayKey.innerText = e.key;
+  displayKeyCode.innerText = e.keyCode;
+});
